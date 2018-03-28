b5a11e96f663b208a499871d2c81e801a76eddf5
karololejarz <karol1olejarz@gmail.com>
2018-03-28 10:57:20 +0200

    Learning to comment

---
+++ 8.4/js/scripts.js
@@ -0,0 +1,6 @@
+// 8.4.2.1. First other comment Learning comments - using Ctrl+/
+var name = prompt('Enter your name');
+alert('Hello, ' + name) /*8.4.2.2. Second other comment/;
+//8.4.1. Commented alert 
+//alert('Witaj, ' + name);
+console.log('Hello, ' + name);
