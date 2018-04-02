7ce92a3efd6e0af5c9ae3a2d4bbac34f918f7e50
karololejarz <karol1olejarz@gmail.com>
2018-04-02 17:44:07 +0200

    8.5 done

---
+++ 8.5/js/script.js
@@ -0,0 +1,8 @@
+var a = 10, h = 10, triangleArea;
+
+a = prompt('Enter triangle base', a);
+h = prompt('Enter triangle height', h);
+
+triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # wprowadzenie-do-js
 Zadanie 8.2 - ze stylami
 Zadanie 8.4
+Zadanie 8.5
