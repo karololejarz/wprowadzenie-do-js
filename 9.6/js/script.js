4211888bb2877d7b2c236f9001876b717522e8ab
karololejarz <karol1olejarz@gmail.com>
2018-04-05 15:28:33 +0200

    Zadanie 9.6

---
+++ 9.6/js/script.js
@@ -0,0 +1,9 @@
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+  var elementNo = document.getElementsByTagName('li').length;
+  element.innerHTML = ('item ' + elementNo);
+  list.appendChild(element);
+});
